3f44167596e5ebd9eb3b49a5290ca743c7c55efc
Ravi Wu <raviwu@gmail.com>
2016-07-24 10:40:22 +0800

    add settings.yml and use config gem to better config the app

--- Gemfile
+++ Gemfile
@@ -27,6 +27,11 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+# Easiest way to add multi-environment yaml settings to Ruby projects.
+gem "config"
+
+gem "safe_yaml", "~>1.0.4"
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
+require 'paperclip/matchers'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -54,4 +55,33 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Include Factory Girl syntax to simplify calls to factories.
+  config.include FactoryGirl::Syntax::Methods
+
+  # Include paperclip's matcher module.
+  config.include Paperclip::Shoulda::Matchers
+
+  config.before :example, perform_enqueued: true do
+    @old_perform_enqueued_jobs =
+      ActiveJob::Base.queue_adapter.perform_enqueued_jobs
+    @old_perform_enqueued_at_jobs =
+      ActiveJob::Base.queue_adapter.perform_enqueued_at_jobs
+    ActiveJob::Base.queue_adapter.perform_enqueued_jobs = true
+    ActiveJob::Base.queue_adapter.perform_enqueued_at_jobs = true
+  end
+
+  config.after :example, perform_enqueued: true do
+    ActiveJob::Base.queue_adapter.perform_enqueued_jobs =
+      @old_perform_enqueued_jobs
+    ActiveJob::Base.queue_adapter.perform_enqueued_at_jobs =
+      @old_perform_enqueued_at_jobs
+  end
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
