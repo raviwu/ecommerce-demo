45eebfa85296fd47873eb63f0a4466c8a527f1a4
Ravi Wu <raviwu@gmail.com>
2016-07-24 11:12:51 +0800

    add currency validation and test

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,4 @@
 class Currency < ActiveRecord::Base
+  validates :name, presence: true
+  validates :symbol, presence: true
 end
--- spec/models/currency_spec.rb
+++ spec/models/currency_spec.rb
@@ -1,5 +1,8 @@
 require "rails_helper"
 
 RSpec.describe Currency, type: :model do
+  subject { create(:currency) }
 
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:symbol) }
 end
