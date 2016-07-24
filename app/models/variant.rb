9ca56cc159872313edf264466490c0c6ee7fc530
Ravi Wu <raviwu@gmail.com>
2016-07-24 12:30:14 +0800

    add custom validator to make sure variant use only valid properties assigned in product

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -9,4 +9,15 @@ class Variant < ActiveRecord::Base
   validates :price, presence: true
   validates :currency, presence: true
   validates :product, presence: true
+
+  validate :properties_conform_to_product
+
+  def properties_conform_to_product
+    return unless product.present?
+    check_fields = properties.keys
+    check_fields.each do |field|
+      allowed_values = product.properties[field]
+      errors.add(field, "allow only '#{allowed_values.join(",")}'") unless allowed_values.include? properties[field]
+    end
+  end
 end
--- spec/models/variant_spec.rb
+++ spec/models/variant_spec.rb
@@ -9,4 +9,39 @@ RSpec.describe Variant, type: :model do
   it { should validate_presence_of(:price) }
   it { should validate_presence_of(:currency) }
   it { should validate_presence_of(:product) }
+
+  describe "validate :properties_conform_to_product" do
+    let!(:product) do
+      create(
+        :product,
+        properties: {
+          color: %w(red black),
+          bandwidth: %w(1G 3G 5G)
+        }
+      )
+    end
+
+    it "is invalid if the variant properties contains invalid values" do
+      variant = build(
+        :variant,
+        product: product,
+        properties: {
+          color: "yellow",
+          bandwidth: "500G"
+        }
+      )
+      expect(variant.valid?).to eq(false)
+    end
+
+    it "is valid if the variant properties use valid values" do
+      variant = build(
+        :variant,
+        properties: {
+          color: "red",
+          bandwidth: "5G"
+        }
+      )
+      expect(variant.valid?).to eq(true)
+    end
+  end
 end
