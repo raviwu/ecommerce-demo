8fa2902c138089d24bb924587a46fc17a7b9934a
Ravi Wu <raviwu@gmail.com>
2016-07-27 18:29:40 +0800

    add line_item and promotion relations and calculation methods

---
+++ app/models/line_items_promotion.rb
@@ -0,0 +1,7 @@
+class LineItemsPromotion < ActiveRecord::Base
+  belongs_to :line_item
+  belongs_to :promotion
+
+  validates :line_item, presence: true
+  validates :promotion, presence: true
+end
--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -14,12 +14,14 @@ RSpec.describe LineItem, type: :model do
   it { should belong_to(:variant) }
   it { should belong_to(:currency) }
   it { should have_many(:inventory_units) }
+  it { should have_many(:line_items_promotions).dependent(:destroy) }
+  it { should have_many(:promotions) }
   it { should validate_presence_of(:order) }
   it { should validate_presence_of(:variant) }
   it { should validate_presence_of(:currency) }
   it { should validate_presence_of(:unit_price) }
   it { should validate_presence_of(:quantity) }
-  it { should validate_presence_of(:lock_inventory) }
+  it { should validate_exclusion_of(:lock_inventory).in_array([nil])}
 
   describe "calculate_line_item_total before_save" do
     let!(:variant) { create(:variant) }
@@ -95,6 +97,64 @@ RSpec.describe LineItem, type: :model do
         line_item.destroy
         expect(InventoryManager.check_free_unit(variant)).to eq(2)
       end
+    end
+  end
+
+  describe "calculate_promo_total" do
+    let!(:variant) { create(:variant) }
+    let!(:line_item) { create(:line_item, variant: variant, unit_price: 100000, quantity: 4, lock_inventory: false) }
+
+    it "discounts when total meets available promotions with no discount on remainer" do
+      promotion = create(
+        :line_item_promotion,
+        description: "相同商品滿 3000 折 300",
+        rule: {
+          type: :discount_on_total_when_total_meets_requirement,
+          require_total: 300000,
+          discount_rate: 0.9,
+          discount_on_remainer: false
+        }
+      )
+      line_item.calculate_promo_total
+      expect(line_item.promo_total).to eq(370000)
+      expect(line_item.reload.promotions).to include(promotion)
+    end
+
+    it "discounts when total meets available promotions with discount on total" do
+      promotion = create(
+        :line_item_promotion,
+        description: "相同商品滿 888 打 85 折",
+        rule: {
+          type: :discount_on_total_when_total_meets_requirement,
+          require_total: 88800,
+          discount_rate: 0.85,
+          discount_on_remainer: true
+        }
+      )
+      line_item.calculate_promo_total
+      expect(line_item.promo_total).to eq((400000 * 0.85).floor)
+      expect(line_item.promotions).to include(promotion)
+    end
+
+    it "discount when quantity meets available promotions" do
+      promotion = create(
+        :line_item_promotion,
+        description: "特定商品每三件打 77 折",
+        rule: {
+          type: :discount_on_total_when_total_meets_requirement,
+          require_quantity: 3,
+          discount_rate: 0.77,
+          included_variant_ids: [variant.id],
+          discount_on_remainer: false
+        }
+      )
+      line_item.calculate_promo_total
+      expect(line_item.promo_total).to eq((300000 * 0.77).floor + 100000)
+      expect(line_item.promotions).to include(promotion)
+      line_item_not_in_discount_variant = create(:line_item, lock_inventory: false)
+      line_item.calculate_promo_total
+      expect(line_item_not_in_discount_variant.promo_total).to be_nil
+      expect(line_item_not_in_discount_variant.promotions).not_to include(promotion)
     end
   end
 end
