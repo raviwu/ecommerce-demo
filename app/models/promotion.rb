b503f57a397af81ed0bb96e7818bd53552512dc3
Ravi Wu <raviwu@gmail.com>
2016-07-28 14:55:11 +0800

    add order promotion calculation mechanism

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -54,6 +54,40 @@ class Promotion < ActiveRecord::Base
       }
   }
 
+  def is_order_promotable?(order, order_total, order_quantity)
+    (is_discount_by_total? && order_total >= rule[:require_total]) || (is_discount_by_quantity? && order_quantity >= rule[:require_quantity])
+  end
+
+  def get_promotable_order_total_and_quantity(order)
+    if rule[:included_product_ids].present?
+      products = Product.where('id IN (?)', rule[:included_product_ids])
+      order_total = order.line_items.where('variant_id IN (?)', products.map(&:variants).flatten.map(&:id))&.map(&:line_item_total).inject(:+)
+      order_quantity = order.line_items.where('variant_id IN (?)', products.map(&:variants).flatten.map(&:id))&.map(&:quantity).inject(:+)
+    elsif rule[:excluded_product_ids].present?
+      excluded_products = Product.where('id IN (?)', rule[:excluded_product_ids])
+      order_total = order.line_items.where('variant_id NOT IN (?)', excluded_products.map(&:variants).flatten.map(&:id))&.map(&:line_item_total).inject(:+)
+      order_quantity = order.line_items.where('variant_id NOT IN (?)', excluded_products.map(&:variants).flatten.map(&:id))&.map(&:quantity).inject(:+)
+    elsif rule[:included_product_ids].blank? && rule[:excluded_product_ids].blank?
+      order_total = order.line_items&.map(&:line_item_total).inject(:+)
+      order_quantity = order.line_items&.map(&:quantity).inject(:+)
+    end
+
+    order_total ||= 0
+    order_quantity ||= 0
+    [order_total, order_quantity]
+  end
+
+  def is_product_promotable?(product)
+    return false if rule[:excluded_product_ids].present? && rule[:excluded_product_ids].include?(product.id)
+    (rule[:included_product_ids].present? && rule[:included_product_ids].include?(product.id)) || \
+      (rule[:included_product_ids].blank? && rule[:excluded_product_ids].blank?)
+  end
+
+  def is_product_meets_promo_requirement?(product, total, quantity)
+    return false unless is_product_promotable?(product)
+    (is_discount_by_total? && total >= rule[:require_total]) || (is_discount_by_quantity? && quantity >= rule[:require_quantity])
+  end
+
   def is_variant_promotable?(variant)
     return false if rule[:excluded_variant_ids].present? && rule[:excluded_variant_ids].include?(variant.id)
     (rule[:included_variant_ids].present? && rule[:included_variant_ids].include?(variant.id)) || \
@@ -73,7 +107,7 @@ class Promotion < ActiveRecord::Base
     rule[:require_quantity].present?
   end
 
-  def calculate_line_item_promo_total_of(line_item, options = {})
+  def calculate_line_item_discount_amount(line_item, options = {})
     return nil unless is_line_item_promotable?(line_item)
 
     total_count_base = line_item.calculate_line_item_total
@@ -92,11 +126,77 @@ class Promotion < ActiveRecord::Base
   def self.assign_promotion_to_line_item(line_item)
     promotion_applied_result =
       active_line_item_promotions.map do |promotion|
-        discount_amount = promotion.calculate_line_item_promo_total_of(line_item).ceil
+        discount_amount = promotion.calculate_line_item_discount_amount(line_item).ceil
         next unless discount_amount.present?
         [promotion.id, discount_amount]
       end
     # Assign only one promotion to LineItem with highest discount_amount
     promotion_applied_result.max_by(&:last)
+  end
+
+  def calculate_product_discount_amount(product, total, quantity, lowest_unit_price)
+    return nil unless is_product_meets_promo_requirement?(product, total, quantity)
+
+    if rule[:discount_on_remainer]
+      total - (total * rule[:discount_rate])
+    elsif is_discount_by_total? && !rule[:discount_on_remainer]
+      deduct_count = total / rule[:require_total]
+      deduct_count * rule[:require_total] * (1 - rule[:discount_rate])
+    elsif is_discount_by_quantity? && !rule[:discount_on_remainer]
+      deduct_count = quantity / rule[:require_quantity]
+      deduct_count * rule[:require_quantity] * lowest_unit_price * (1 - rule[:discount_rate])
+    end
+  end
+
+  def calculate_order_discount_amount(order)
+    order_total, order_quantity = get_promotable_order_total_and_quantity(order)
+    return nil unless is_order_promotable?(order, order_total, order_quantity)
+
+    if rule[:discount_on_remainer]
+      order_total - (order_total * rule[:discount_rate])
+    elsif is_discount_by_total? && !rule[:discount_on_remainer]
+      deduct_count = order_total / rule[:require_total]
+      deduct_count * rule[:require_total] * (1 - rule[:discount_rate])
+    end
+
+    # TODO: Handle "Buy X get O free condition"
+  end
+
+  def self.assign_promotion_to_product(product, total, quantity, lowest_unit_price)
+    promotion_applied_result =
+      active_product_promotions.map do |promotion|
+        discount_amount = promotion.calculate_product_discount_amount(product, total, quantity, lowest_unit_price)&.ceil
+        next unless discount_amount.present?
+        [promotion.id, product.id, discount_amount]
+      end
+    # Assign only one promotion to Product with highest discount_amount
+    promotion_applied_result.compact.max_by(&:last)
+  end
+
+  def self.assign_product_promotion_to_order(order)
+    return unless Promotion.active_product_promotions.present?
+
+    products = order.line_items.map(&:variant).map(&:product).uniq
+
+    products.map do |product|
+      line_items = order.line_items.where('variant_id IN (?)', product.variants.ids)
+      total = line_items.map(&:line_item_total).compact.inject(&:+)
+      quantity = line_items.map(&:quantity).compact.inject(&:+)
+      lowest_unit_price = line_items.min_by(&:unit_price).unit_price
+
+      assign_promotion_to_product(product, total, quantity, lowest_unit_price)
+    end.compact
+  end
+
+  def self.assign_order_promotion_to_order(order)
+    return unless Promotion.active_order_promotions.present?
+    promotion_applied_result =
+      active_order_promotions.map do |promotion|
+        discount_amount = promotion.calculate_order_discount_amount(order)&.ceil
+        next unless discount_amount.present?
+        [promotion.id, promotion.rule[:included_product_ids], promotion.rule[:excluded_product_ids], discount_amount]
+      end
+    # Assign only one promotion to Order with highest discount_amount
+    promotion_applied_result.compact.max_by(&:last)
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -19,4 +19,88 @@ RSpec.describe Order, type: :model do
   it { should validate_presence_of(:line_item_total) }
   it { should validate_presence_of(:promo_total) }
   it { should validate_presence_of(:status) }
+
+  describe "calculate_promo_total" do
+    let!(:product) { create(:product) }
+    let!(:variant_1) { create(:variant, product: product) }
+    let!(:line_item_1) { create(:line_item, variant: variant_1, unit_price: 100000, quantity: 4, lock_inventory: false) }
+    let!(:variant_2) { create(:variant, product: product) }
+    let!(:line_item_2) { create(:line_item, variant: variant_2, unit_price: 100000, quantity: 3, lock_inventory: false) }
+
+    let!(:variant_3) { create(:variant) }
+    let!(:line_item_3) { create(:line_item, variant: variant_3, unit_price: 10000, quantity: 3, lock_inventory: false) }
+
+    let!(:order) { create(:order) }
+
+    before(:each) do
+      order.line_items << line_item_1
+      order.line_items << line_item_2
+      order.line_items << line_item_3
+    end
+
+    it "discounts when total meets available promotions with no discount on remainer" do
+      product_promotion = create(
+        :product_promotion,
+        description: "相同商品滿 3000 折 300",
+        rule: {
+          type: :discount_on_total_when_total_meets_requirement,
+          included_product_ids: [product.id],
+          require_total: 300000,
+          discount_rate: 0.9,
+          discount_on_remainer: false
+        }
+      )
+      order.calculate_promo_total
+      expect(order.promo_total).to eq(670000)
+      expect(order.promotions).to include(product_promotion)
+      expect(order.orders_promotions.last.discount_amount).to eq(60000)
+    end
+
+    it "discounts when total meets available promotions with discount on total" do
+      product_promotion = create(
+        :product_promotion,
+        description: "相同商品滿 888 打 85 折",
+        rule: {
+          type: :discount_on_total_when_total_meets_requirement,
+          excluded_product_ids: [product.id],
+          require_total: 88800,
+          discount_rate: 0.85,
+          discount_on_remainer: true
+        }
+      )
+      order_promotion = create(
+        :order_promotion,
+        description: "滿 199 打 8 折（部分商品除外）",
+        rule: {
+          type: :discount_on_total_when_total_meets_requirement,
+          excluded_product_ids: [product.id],
+          require_total: 19900,
+          discount_rate: 0.8,
+          discount_on_remainer: true
+        }
+      )
+      order.calculate_promo_total
+      expect(order.promo_total).to eq(724000)
+      expect(order.promotions).to include(order_promotion)
+      expect(order.promotions).not_to include(product_promotion)
+      expect(order.orders_promotions.last.discount_amount).to eq(6000)
+    end
+
+    it "discount when quantity meets available promotions" do
+      product_promotion = create(
+        :product_promotion,
+        description: "特定商品每三件打 77 折",
+        rule: {
+          type: :discount_on_total_when_total_meets_requirement,
+          included_product_ids: [product.id],
+          require_quantity: 3,
+          discount_rate: 0.77,
+          discount_on_remainer: false
+        }
+      )
+      order.calculate_promo_total
+      expect(order.promo_total).to eq((600000 * 0.77).floor + 100000 + 30000)
+      expect(order.promotions).to include(product_promotion)
+    end
+  end
 end
